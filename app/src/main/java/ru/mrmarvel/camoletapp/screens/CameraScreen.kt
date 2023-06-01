3584ab2252409fafd10b50f7b604c9f03c1b71ac
MrMarvel <seregakkk999@yandex.ru>
2023-06-01 20:30:06 +0300

    Trying to make saving video Not successful

--- app/src/main/java/ru/mrmarvel/camoletapp/screens/CameraScreen.kt
+++ app/src/main/java/ru/mrmarvel/camoletapp/screens/CameraScreen.kt
@@ -1,7 +1,23 @@
 package ru.mrmarvel.camoletapp.screens
 
 import android.Manifest
+import android.app.Service
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraProvider
+import androidx.camera.core.CameraSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.MediaStoreOutputOptions
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.fadeIn
@@ -16,6 +32,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.IconButton
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,11 +40,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.LifecycleOwner
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.relay.compose.BoxScopeInstanceImpl.align
@@ -42,6 +63,10 @@ import ru.mrmarvel.camoletapp.ui.CameraFragment
 import ru.mrmarvel.camoletapp.ui.MOPFragment
 import ru.mrmarvel.camoletapp.ui.RoomFragment
 import ru.mrmarvel.camoletapp.ui.TopLeftBar
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.Executors
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -53,7 +78,8 @@ fun CameraScreen(
     val permissions = mutableListOf(
         Manifest.permission.CAMERA,
         Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.RECORD_AUDIO
     )
     permissions += if (Build.VERSION.SDK_INT <= 28){
         listOf(
@@ -90,6 +116,7 @@ fun CameraScreen(
     ) {
         if (permissionState.allPermissionsGranted){
             CameraFragment(yolov8Ncnn = yolov8Ncnn)
+
         }
         AnimatedVisibility(visible = isRecordingStarted.value) {
             AnimatedVisibility(visible = isMOPSelected.value) {
@@ -141,9 +168,18 @@ fun CameraScreen(
                                 false -> Action.START
                                 true -> Action.STOP
                             }
+                            val lifecycleOwner = LocalLifecycleOwner.current
+                            val recording: MutableState<Recording?> = remember { mutableStateOf(null)}
                             CameraButton(action = action, onItemClick = {
                                 if (isRecordingStarted.value) {
+                                    recording.value?.stop()
+                                    recording.value = null
                                     navigateToObserveResultScreen()
+                                } else {
+                                    recording.value = cameraStart(
+                                        context = context,
+                                        lifecycleOwner = lifecycleOwner
+                                    )
                                 }
                                 isRecordingStarted.value = !isRecordingStarted.value
                             })
@@ -161,6 +197,50 @@ fun CameraScreen(
     }
 }
 
+private fun cameraStart(context: Context, lifecycleOwner: LifecycleOwner): Recording? {
+    val cameraExecutor = Executors.newSingleThreadExecutor()
+    val recorder = Recorder.Builder()
+        .setExecutor(cameraExecutor).setQualitySelector(QualitySelector.from(Quality.LOWEST))
+        .build()
+    val videoCapture = VideoCapture.withOutput(recorder)
+    val cameraProvider = ProcessCameraProvider.getInstance(context).get()
+    try {
+        // Bind use cases to camera
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner, CameraSelector.DEFAULT_BACK_CAMERA, videoCapture)
+    } catch(exc: Exception) {
+        Log.e("MYDEBUG", "Use case binding failed", exc)
+    }
+    // Create MediaStoreOutputOptions for our recorder
+    val FILENAME_FORMAT = "dd"
+    val name = "CameraX-recording-" +
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                .format(System.currentTimeMillis()) + ".mp4"
+    val contentValues = ContentValues().apply {
+        put(MediaStore.Video.Media.DISPLAY_NAME, name)
+    }
+    val mediaStoreOutput = MediaStoreOutputOptions.Builder(context.contentResolver,
+        MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+        .setContentValues(contentValues)
+        .build()
+
+// 2. Configure Recorder and Start recording to the mediaStoreOutput.
+    if (ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.RECORD_AUDIO
+        ) == PackageManager.PERMISSION_GRANTED
+    ) {
+        val recording =
+            videoCapture.output
+                .prepareRecording(context, mediaStoreOutput)
+                .withAudioEnabled()
+                .start(ContextCompat.getMainExecutor(context)) {
+                    // Toast.makeText(context, "ТЕСТ", Toast.LENGTH_SHORT).show()
+                }
+        return recording
+    }
+    return null
+}
 @Composable
 fun FlatChangeWindow(
     cameraViewModel: CameraScreenViewModel,
